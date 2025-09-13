b5596643329de6a02f00b015c99dbc67b06da3d0
Việt Anh Nguyễn Vũ <ACER@MacBook-Pro.local>
2025-09-14 04:25:54 +0700

    Finish global map view

--- src/MapView.vue
+++ src/MapView.vue
@@ -1,10 +1,30 @@
 <script setup>
+import { onMounted, ref } from "vue";
+import L from "leaflet";
 import NavigateButton from "./components/NavigateButton.vue";
+
+const mapContainer = ref(null);
+
+onMounted(() => {
+  const map = L.map(mapContainer.value).setView([0, 0], 2);
+
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+});
 </script>
 
 <template>
   <NavigateButton to="/home" title="Back" icon="←" />
-  <p>map here</p>
+  <div ref="mapContainer" id="fullMap"></div>
 </template>
 
-<style scoped></style>
+<style scoped>
+#fullMap {
+  margin-top: 10px;
+  width: 100%;
+  height: 85vh;
+  max-height: 85vh;
+}
+</style>
